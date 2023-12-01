897ecfa2d085ca997181fb45d503be7460e32a31
Neilos <neil.atkinson@smartpension.co.uk>
2023-12-01 11:49:49 +0000

    Use more idiomatic sum

--- day_1/script.rb
+++ day_1/script.rb
@@ -3,13 +3,13 @@
 
 require 'pry'
 
-sum_of_calibration_values = File.foreach('./input.txt').reduce(0) do |sum, line|
+sum_of_calibration_values = File.foreach('./input.txt').sum(0) do |line|
   string_ints = line.scan(/\d/)
   calibration_value = string_ints.first + string_ints.last
 
   raise 'invalid calibration_value' unless calibration_value.length == 2
 
-  sum + calibration_value.to_i
+  calibration_value.to_i
 end
 
 puts sum_of_calibration_values
